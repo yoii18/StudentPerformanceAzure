ac02b50e12176b37369facc2b8a97c8732cb8734
yoii18 <dpssalhotra@gmail.com>
2023-08-31 03:35:58 +0530

    front end work underway

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ def index():
     return render_template('index.html')
 
 @app.route('/predictdata', methods = ['GET', 'POST'])
+@app.route('/home.html', methods = ['GET', 'POST'])
 def predict_datapoint():
 
     if request.method == 'GET':
@@ -36,4 +37,4 @@ def predict_datapoint():
         return render_template('home.html', results = results[0])
     
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0')
+    app.run(host = '0.0.0.0', debug=True)
