60216650cfb2bac1416a9d252f271351e2c9be96
yoii18 <dpssalhotra@gmail.com>
2023-09-08 20:04:51 +0530

    front end changes

--- app.py
+++ app.py
@@ -10,10 +10,20 @@ app = application
 
 # Route
 @app.route('/')
+@app.route('/index.html')
 def index():
     return render_template('index.html')
 
-@app.route('/predictdata', methods = ['GET', 'POST'])
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/prediction.html', methods = ['GET', 'POST'])
 @app.route('/home.html', methods = ['GET', 'POST'])
 def predict_datapoint():
 
@@ -37,4 +47,4 @@ def predict_datapoint():
         return render_template('home.html', results = results[0])
     
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', debug=True)
+    app.run(host = '0.0.0.0') #, debug=True)
